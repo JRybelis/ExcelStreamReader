377faa9713c703ae32f19d233ef02f147009ca7a
Jokūbas Rybelis <jokubas.rybelis@softra.lt>
2022-04-14 15:05:30 +0300

    Add generic base controller + LTC controller.

---
+++ ExcelStreamReaderConsole/Controllers/GenericControllerBase.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using CoreData.Dtos.LtCustomers;
+using CoreData.Entities.LtCustomers;
+using ExcelStreamReaderConsole.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelStreamReaderConsole.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GenericControllerBase<TDto, TEntity> : ControllerBase
+    where TDto : LtCustomersDto where TEntity : LtCustomers
+{
+    private readonly IMapper _mapper;
+    private readonly IGenericRepository<TEntity> _repository;
+
+    public GenericControllerBase(IMapper mapper, IGenericRepository<TEntity> repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public virtual async Task<IEnumerable<TDto>> GetAll()
+    {
+        var entities = await _repository.GetAll();
+        return _mapper.Map<IEnumerable<TDto>>(entities);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<TDto> GetById(int id)
+    {
+        var entity = await _repository.GetById(id);
+        return _mapper.Map<TDto>(entity);
+    }
+
+    [HttpPost]
+    public async Task Upsert(TDto dto)
+    {
+        var entity = _mapper.Map<TEntity>(dto);
+        await _repository.Upsert(entity);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete(int id)
+    {
+        await _repository.Delete(id);
+    }
+}
---
+++ ExcelStreamReaderConsole/Controllers/LtCustomerController.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreData.Dtos.LtCustomers;
+using CoreData.Entities.LtCustomers;
+using ExcelStreamReaderConsole.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelStreamReaderConsole.Controllers;
+
+[ApiController]
+[Microsoft.AspNetCore.Components.Route("[controller]")]
+public class LtCustomerController : GenericControllerBase<LtCustomersDto, LtCustomers>
+{
+    private readonly IMapper _mapper;
+    private readonly IGenericRepository<LtCustomers> _repository;
+
+    public LtCustomerController(IGenericRepository<LtCustomers> repository, IMapper mapper) : base(mapper, repository)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public override async Task<IEnumerable<LtCustomersDto>> GetAll()
+    {
+        var entities = await _repository.GetAll();
+        var dtos = _mapper.Map<IEnumerable<LtCustomersDto>>(entities);
+        return dtos;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<LtCustomers> GetById(int id)
+    {
+        var entity = await _repository.GetById(id);
+        return _mapper.Map<LtCustomers>(entity);
+    }
+
+    [HttpPost]
+    public async Task Upsert(LtCustomersDto dto)
+    {
+        var entity = _mapper.Map<LtCustomers>(dto);
+        await _repository.Upsert(entity);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task Delete(int id)
+    {
+        await _repository.Delete(id);
+    }
+    
+}
